097adf8bfef8005867e0b7c7080645ed7978f7b2
Amasha <amashagalhenage@gmail.com>
2024-09-23 03:18:10 +0530

    form submission has implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AvailabilityCalendar from "./components/Calendar/AvailabilityCalendar";
+import SubmissionForm from "./components/Form/SubmissionForm";
 
 function App() {
   return (
     <div className="App">
-      <AvailabilityCalendar/>
+      {/* <AvailabilityCalendar/> */}
+      <SubmissionForm/>
     </div>
   );
 }
