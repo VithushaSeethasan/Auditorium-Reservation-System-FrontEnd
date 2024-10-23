c2a829f4c85b7e33f7d15eb03d9d3fe46e859f47
chathu326 <chamikachathurangi2021@gmail.com>
2024-10-23 20:19:29 +0530

    Add pages

---
+++ src/pages/SignIn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Nav/NavSignIn";
+import SignIn from "../components/Auth/SignIn";
+
+const SignInPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInPage
---
+++ src/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Nav/NavSignIn";
+import SignUp from "../components/Auth/SignUp";
+
+const SignUpPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <SignUp />
+        </div>
+    )
+}
+
+export default SignUpPage
